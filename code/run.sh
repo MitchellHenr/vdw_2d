8719f865e4029076b5c8640cdde16ff7967a703a
MitchellHenr <henry@tom.org>
2017-05-13 17:58:35 -0400

    Shipping to desktop

--- code/run.sh
+++ code/run.sh
@@ -1,17 +1,18 @@
-#! /usr/local/bin/bash
+#! /usr/bin/bash
 set -e
 
 FILE="$( dirname "${BASH_SOURCE[0]}" )/vdw.py"
 
-# ATOMS=( H He Na )
-# MATERIALS=( MoS2 graphene )
+ATOMS=( H He Na )
+MATERIALS=( MoS2 graphene )
 
-# for atom in "${ATOMS[@]}"
-# do
-# 	for material in "${MATERIALS[@]}"
-# 	do
-# 		echo $material $atom
-# 		python3 $FILE -g $atom $material
-# 	done
-# done
-python3 $FILE -g H MoS2
+for atom in "${ATOMS[@]}"
+do
+	for material in "${MATERIALS[@]}"
+	do
+		echo $material $atom
+		python3 $FILE -g $atom $material
+	done
+done
+
+say "Your van der Waals forces are ready"
